e2baaafff97e660cbcb1043c614ff69f40c1916c
hogelog <konbu.komuro@gmail.com>
2025-03-19 02:42:19 +0900

    Add generate job

---
+++ app/jobs/html_generate_job.rb
@@ -0,0 +1,26 @@
+require "fileutils"
+
+class HtmlGenerateJob < ApplicationJob
+  def perform
+    contributors = Contributor
+      .select("contributors.*, COUNT(commits.id) as commits_count")
+      .left_joins(:commits)
+      .group(:id)
+      .order(commits_count: :desc)
+
+    index_html = ApplicationController.renderer.render_to_string(
+      template: "public/contributors/index",
+      locals: { contributors: }
+    )
+    File.write("tmp/public/index.html", index_html)
+
+    FileUtils.rm_rf("tmp/public/contributors/*.html")
+    contributors.each do |contributor|
+      show_html = ApplicationController.renderer.render_to_string(
+        template: "public/contributors/show",
+        locals: { contributor: }
+      )
+      File.write("tmp/public/contributors/#{contributor.name_path}.html", show_html)
+    end
+  end
+end
--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -42,4 +42,32 @@ class Contributor < ApplicationRecord
     contributor.contributor_names.find_or_create_by!(name:)
     contributor
   end
+
+  def bot?
+    return true if emails.include?("(no author)@b2dd03c8-39d4-4d8f-98ff-823fe69b080e")
+    return true if logins.include?("matzbot")
+    return true if emails.include?("license.update@rubygems.org")
+    return true if emails.include?("test@ruby-lang.org")
+    false
+  end
+
+  def emails
+    @emails ||= contributor_emails.map(&:email)
+  end
+
+  def names
+    @names ||= contributor_names.map(&:name)
+  end
+
+  def logins
+    @logins ||= contributor_logins.map(&:login)
+  end
+
+  def latest_name
+    commits.last&.name
+  end
+
+  def name_path
+    @name_path ||= URI.encode_uri_component(latest_name.downcase.gsub(" ", "-"))
+  end
 end 
