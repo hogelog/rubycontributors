341520a351da408a4b6dce67e5314f13a03d2e46
hogelog <konbu.komuro@gmail.com>
2025-03-19 23:27:23 +0900

    Add preview controllers

---
+++ app/controllers/preview/contributors_controller.rb
@@ -0,0 +1,16 @@
+class Preview::ContributorsController < ApplicationController
+  def index
+    contributors = Contributor
+      .select("contributors.*, COUNT(commits.id) as commits_count")
+      .left_joins(:commits)
+      .group(:id)
+      .order(commits_count: :desc)
+
+    render "public/contributors/index", locals: { contributors: }
+  end
+
+  def show
+    contributor = ContributorName.find_by_name!(params[:id]).contributor
+    render "public/contributors/show", locals: { contributor: }
+  end
+end 
--- app/models/contributor_name.rb
+++ app/models/contributor_name.rb
@@ -1,3 +1,8 @@
 class ContributorName < ApplicationRecord
   belongs_to :contributor
+
+  def self.find_by_name!(name)
+    candidates = [name, name.gsub("-", " "), name.titleize]
+    find_by!(name: candidates)
+  end
 end 
